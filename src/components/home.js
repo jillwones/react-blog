0ee3bc65eb74984808e6d6881aeecafa3d6515f1
williamjones <willj836@gmail.com>
2023-01-04 17:12:45 +0000

    error messages

--- src/components/home.js
+++ src/components/home.js
@@ -1,24 +1,39 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./blogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-
-  const [name, setName] = useState("Mario")
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
-      .then((response) => response.json())
-      .then((data) => setBlogs(data))
+      .then((res) => {
+        if (!res.ok) {
+          // error coming back from server
+          throw Error("could not fetch the data for that resource");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setIsPending(false);
+        setBlogs(data);
+        setError(null);
+      })
+      .catch((err) => {
+        // auto catches network / connection error
+        setIsPending(false);
+        setError(err.message);
+      });
   }, []);
 
   return (
     <div className="home">
-      { blogs ? <BlogList blogs={blogs} title="All Blogs" /> : <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
-}
+};
 
 export default Home;
-
- 
