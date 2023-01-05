06c6f9c9e2ac566772f9d82d684b405d1f05a89a
williamjones <willj836@gmail.com>
2023-01-05 09:51:41 +0000

    routes and router links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Navbar from "./components/navbar";
 import Home from "./components/home";
-
+import Create from "./components/create";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/create" element={<Create />}/>
+          </Routes>
+        </div>
       </div>
-    </div>
   );
 }
 
