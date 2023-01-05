2aa581b1bbca01a4b9e44f34da3b26a8c4b57341
williamjones <willj836@gmail.com>
2023-01-05 10:49:40 +0000

    useNavigate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import Navbar from "./components/navbar";
 import Home from "./components/home";
 import Create from "./components/create";
+import BlogDetails from "./components/blogDetails";
+
 import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
       <div className="App">
@@ -10,6 +13,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/create" element={<Create />}/>
+            <Route path="/blogs/:id" element={<BlogDetails />}/>
           </Routes>
         </div>
       </div>
